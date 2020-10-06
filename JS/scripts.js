518d91cef3ea18ef5ece9a8749ad332d51956707
Altyn15 <altynnikov313@gmail.com>
2020-10-07 02:35:31 +0300

    add burger menu page index

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,29 +1,5 @@
-$(document).ready(function(){
-
-    $('.owl-carousel').owlCarousel({
-
-
-        loop:true,
-        autoplay: true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
-            },
-
-            1000:{
-                items:1
-            }
-
-        }
-
-    });
-});
-$( function() {
-    $( "#dialog" ).dialog({
+$(function () {
+    $("#dialog").dialog({
         autoOpen: false,
         show: {
             effect: "blind",
@@ -35,7 +11,53 @@ $( function() {
         }
     });
 
-    $( "#opener" ).on( "click", function() {
-        $( "#dialog" ).dialog( "open" );
+    $("#opener").on("click", function () {
+        $("#dialog").dialog("open");
     });
-} );
+});
+
+$(document).ready(function () {
+
+
+    $('.owl-carousel').owlCarousel({
+
+
+        loop: true,
+        autoplay: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1000: {
+                items: 1
+            }
+
+        }
+
+    });
+});
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('nav');
+var lineOne = document.querySelector('nav .menu-btn .line--1');
+var lineTwo = document.querySelector('nav .menu-btn .line--2');
+var lineThree = document.querySelector('nav .menu-btn .line--3');
+var link = document.querySelector('nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in')
+})
+
+
+
+
+
+
+
